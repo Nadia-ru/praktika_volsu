5999d5a2e181d3592a1cefbddafcb664d5ae9538
nadia.trfm <69869414+Nadia-ru@users.noreply.github.com>
2023-02-23 15:52:07 +0300

    button

--- lib/widgets/auth_widget.dart
+++ lib/widgets/auth_widget.dart
@@ -42,6 +42,25 @@ class _AuthWidgetState extends State<AuthWidget> {
               children: [],
             ),
             const _FormWidget(),
+            const SizedBox(height: 24),
+            SizedBox(
+              width: double.infinity,
+              height: 48,
+              child: TextButton(
+
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Color(0xFF136DEC)),
+                  foregroundColor: MaterialStateProperty.all(Color(0xFFFFFFFF)),
+                  textStyle: MaterialStateProperty.all(TextStyle(fontSize: 16,fontWeight: FontWeight.w400,)),
+                  padding: MaterialStateProperty.all(EdgeInsets.symmetric(vertical: 16.0)),
+                    shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12.0),
+                    )),
+                ),
+
+                  onPressed: (){},
+                  child: Text('Дальше')),
+            )
           ],
         ),
       ),
@@ -71,7 +90,9 @@ class _FormWidgetStateEmail extends State<_FormWidget> {
               hintStyle: textStyleStart,
               border: OutlineInputBorder(
                   borderRadius: BorderRadius.all(Radius.circular(12.0)),
-                  borderSide: BorderSide(color: Color(0xFFE3E7EC), width: 1.0))),
+                  borderSide: BorderSide(color: Color(0xFFE3E7EC), width: 1.0)),
+              contentPadding: EdgeInsets.symmetric(horizontal: 12)
+          ),
         ),
         SizedBox(height: 8,),
         TextField(
@@ -83,7 +104,7 @@ class _FormWidgetStateEmail extends State<_FormWidget> {
                 borderRadius: BorderRadius.all(Radius.circular(12.0)),
                 borderSide: BorderSide(color: Color(0xFFE3E7EC), width: 1.0),
             ),
-
+            contentPadding: EdgeInsets.symmetric(horizontal: 12)
           ),
         ),
       ],
